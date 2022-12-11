e723c50e8d918328de9b8c4df79ecc44a9bcb893
DavidHunterScot <david.hunter@echoverse.cc>
2022-12-11 15:47:07 +0000

    Created Siteicly PHP Class
    This class gets initialised with two string parameters being the input path and output path.
    
    The output directory will be emptied upon each execution, before being filled with the new and updated static content. This is to ensure any deleted directory contents since last execution are reflected also in output.
    
    All PHP files will be staticified (PHP to HTML), and anything else simply copied from input directory to output directory.
    
    Files which contain ".inc" within their name will be ignored to allow for inclusion within PHP should this be desired. Meaning if you had an index.php file which included a header.inc.php and footer.inc.php file, the header and footer files would be included in the index file, but not staticified themselves.

--- Siteicly.php
+++ Siteicly.php
@@ -2,5 +2,116 @@
 
 class Siteicly
 {
+    public function __construct( String $inputPath, String $outputPath )
+    {
+        if( ! is_dir( $inputPath ) )
+            die( "Input Path does not exist or is not a directory!\n" );
+        
+        $this->emptyDir( $outputPath );
+        $this->staticify( $inputPath, $outputPath );
+    }
+
+    private function staticify( String $inputPath, String $outputPath ): void
+    {
+        if( ! is_dir( $inputPath ) )
+            die( "Staticify Input Path does not exist or is not a directory!\n" );
+        
+        if( ! is_dir( $outputPath ) )
+        {
+            mkdir( $outputPath );
+            echo "Created output path: " . $outputPath . "\n";
+        }
+
+        $inputItems = scandir( $inputPath );
+
+        if( count( $inputItems ) <= 2 )
+            echo "Input directory is empty. Nothing to output.\n";
+
+        foreach( $inputItems as $inputItem )
+        {
+            if( $inputItem == "." || $inputItem == ".." )
+                continue;
+            
+            $inputFilePath = $inputPath . DIRECTORY_SEPARATOR . $inputItem;
+            $outputFilePath = $outputPath . DIRECTORY_SEPARATOR . $inputItem;
+            
+            if( is_dir( $inputFilePath ) )
+            {
+                $this->staticify( $inputFilePath, $outputFilePath );
+                continue;
+            }
+
+            if( is_file( $inputFilePath ) )
+            {
+                if( str_contains( $inputFilePath, ".inc" ) )
+                    continue;
+
+                if( substr( $inputFilePath, -4 ) == ".php" )
+                {
+                    $outputFilePath = str_replace( ".php", ".html", $outputFilePath );
+
+                    $this->staticifyPHP( $inputFilePath, $outputFilePath );
+                    echo "Staticified PHP File: " . $inputFilePath . " to " . $outputFilePath . "!\n";
+                    continue;
+                }
+
+                copy( $inputFilePath, $outputFilePath );
+                echo "Copied file " . $inputFilePath . " to " . $outputFilePath . "!\n";
+            }
+        }
+    }
+
+    private function staticifyPHP( String $inputFilePath, String $outputFilePath ): void
+    {
+        if( ! is_file( $inputFilePath ) )
+            die( "Staticify PHP Input File Path does not exist or is not a file!\n" );
+        
+        ob_start();
     
+        include( $inputFilePath );
+        
+        $page = ob_get_contents();
+        ob_end_clean();
+        
+        @chmod( $outputFilePath, 0755 );
+        $fw = fopen( $outputFilePath, "w" );
+        fputs( $fw, $page, strlen( $page ) );
+        fclose( $fw );
+    }
+
+    private function emptyDir( String $dirPath )
+    {
+        if( ! is_dir( $dirPath ) )
+            die( "Cannot empty non-existant directory: " . $dirPath . "\n" );
+        
+        $dirItems = scandir( $dirPath );
+
+        if( count( $dirItems ) <= 2 )
+            echo "Ignoring already empty directory: " . $dirPath;
+        
+        foreach( $dirItems as $dirItem )
+        {
+            if( $dirItem == "." || $dirItem == ".." )
+                continue;
+            
+            $dirItemPath = $dirPath . DIRECTORY_SEPARATOR . $dirItem;
+            
+            if( is_file( $dirItemPath ) )
+            {
+                unlink( $dirItemPath );
+                echo "Removed file: " . $dirItemPath . "\n";
+            }
+
+            if( is_dir( $dirItemPath ) )
+            {
+                $this->emptyDir( $dirItemPath );
+                rmdir( $dirItemPath );
+                echo "Removed dir: " . $dirItemPath . "\n";
+            }
+        }
+    }
 }
+
+header( "Content-Type: text/plain" );
+
+new Siteicly( __DIR__ . DIRECTORY_SEPARATOR . "input", __DIR__ . DIRECTORY_SEPARATOR . "output" );
